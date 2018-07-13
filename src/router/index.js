43edef53bff72c3c3c4dd9affd960b3be10aeab5
毛金山 <maojs@mapbar.com>
2018-07-13 10:34:04 +0800

    adf

--- src/router/index.js
+++ src/router/index.js
@@ -4,9 +4,14 @@ import HelloWorld from "@/components/HelloWorld";
 
 Vue.use(Router);
 
+function getAbsolutePath() {
+  let path = location.pathname;
+  return path.substring(0, path.lastIndexOf("/") + 1);
+}
+
 export default new Router({
   mode: "history",
-  base: "/dist/",
+  base: getAbsolutePath(),
   routes: [
     {
       path: "/",
